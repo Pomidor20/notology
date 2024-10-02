d48a8bd1cb8f43084fd7faa105fb2560d81d6964
Owner Windows <s161988@gmail.com>
2024-10-02 22:12:51 +0300

    4 Задание

---
+++ Netology/17.Виртуализация/17.5 docker-in-practice/files/clone/bash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir /opt/hw/
+cd /opt/hw/
+git clone https://github.com/Pomidor20/notology.git
+cd ./notology/Netology/17.Виртуализация/17.5\ docker-in-practice/files/clone/
+docker compose up -d

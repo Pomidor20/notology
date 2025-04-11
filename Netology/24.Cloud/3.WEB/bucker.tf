b8d7d34735dbaae6a0a400bb2c634c4ccd2c775c
Pomidor20 <92988958+Pomidor20@users.noreply.github.com>
2025-04-11 19:40:08 +0300

    Update bucker.tf

--- Netology/24.Cloud/3.WEB/bucker.tf
+++ Netology/24.Cloud/3.WEB/bucker.tf
@@ -28,12 +28,12 @@ resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
 }
 # Создание KMS ключа
 
-#resource "yandex_kms_symmetric_key" "key-a" {
-#  name              = "example-symetric-key"
-#  description       = "Pomidorkinkey"
-#  default_algorithm = "AES_128"
-#  rotation_period   = "8760h" // equal to 1 year
-#}
+resource "yandex_kms_symmetric_key" "key-a" {
+  name              = "example-symetric-key"
+  description       = "Pomidorkinkey"
+  default_algorithm = "AES_128"
+  rotation_period   = "8760h" // equal to 1 year
+}
 
 # Создание бакета с использованием статического ключа
 # https://yandex.cloud/ru/docs/storage/operations/buckets/bucket-availability
@@ -61,14 +61,14 @@ resource "yandex_storage_bucket" "pomidorbucket" {
     config_read = true
   }
 
-#  server_side_encryption_configuration {
-#    rule {
-#      apply_server_side_encryption_by_default {
-#        kms_master_key_id = yandex_kms_symmetric_key.key-a.id
-#        sse_algorithm     = "aws:kms"
-#      }
-#  
-#    }
-#  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = yandex_kms_symmetric_key.key-a.id
+       sse_algorithm     = "aws:kms"
+      }
+  
+    }
+  }
 #  depends_on = [yandex_cm_certificate.pomidorkincert]
 }
